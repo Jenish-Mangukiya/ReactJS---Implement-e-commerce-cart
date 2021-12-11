119cbd5f96021808da0d15a1d13f69c95aa48b89
Jenish-Mangukiya <85919929+Jenish-Mangukiya@users.noreply.github.com>
2021-12-12 03:02:48 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,27 @@
+//import App from "./App";
+
+const data = {
+    products:[
+        {
+            id:'1',
+            name:'Mackbook',
+            price:1400,
+            image:"https://i.picsum.photos/id/0/5616/3744.jpg?hmac=3GAAioiQziMGEtLbfrdbcoenXoWAW-zlyEAMkfEdBzQ",
+            
+        },
+        {
+            id:'2',
+            name:'Solidwood Dining Set',
+            price:5500,
+            image:"https://i.picsum.photos/id/625/2507/1674.jpg?hmac=ZuCWygmEqFbv5q0hlD-jimDTtYWRwgAebHLepuXaUUk",
+        },
+        {
+            id:'3',
+            name:'British_Mug',
+            price:1000,
+            image:'https://i.picsum.photos/id/30/1280/901.jpg?hmac=A_hpFyEavMBB7Dsmmp53kPXKmatwM05MUDatlWSgATE',
+        },
+        
+    ],
+};
+export default data;
